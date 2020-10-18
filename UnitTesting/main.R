665758f1f8e43b9381dd53f34d25a09569ce18aa
Freddy Drennan <drennanfreddy@gmail.com>
2020-10-18 13:55:17 -0600

    update

--- UnitTesting/main.R
+++ UnitTesting/main.R
@@ -1,13 +1,18 @@
+
+# References --------------------------------------------------------------
+
+## https://medium.com/@JeffLombardJr/opinion-when-should-i-write-unit-tests-164b245bffbc
+
 # Functions ---------------------------------------------------------------
 
-#' factor_conversion
+#' as_factor_dataframe
 #' @importFrom dplyr as_tibble
 #' @importFrom dplyr mutate_at
 #' @importFrom forcats as_factor
 #' @importFrom magrittr not
 #' @param cars_df A renaming of the mtcars dataset
-#' @export factor_conversion
-factor_conversion <- function(df = NULL, columns = NULL) {
+#' @export as_factor_dataframe
+as_factor_dataframe <- function(df = NULL, columns = NULL) {
 
   df_check <- is.data.frame(df)
   if (not(df_check)) {
@@ -21,13 +26,21 @@ factor_conversion <- function(df = NULL, columns = NULL) {
 
   df %>%
     mutate_at(
-      vars(columns),
-      as_factor
+      all_of(columns),
+      as.factor
     )
 
 }
 
-
+#' build_model
+#' @param df A renaming of the mtcars dataset
+#' @param x Predictor column, character
+#' @param y Response column, character
+#' @export build_model
+build_model <- function(df = NULL, x = NULL , y = NULL) {
+  model_form <- as.formula(glue('{y} ~ {x}'))
+  glm(model_form, data = df, family=binomial(link="logit"))
+}
 
 # Libraries ---------------------------------------------------------------
 
@@ -35,12 +48,17 @@ factor_conversion <- function(df = NULL, columns = NULL) {
 library(dplyr)
 library(magrittr)
 library(forcats)
+library(glue)
+
+# 1. Create a function which converts columns to factors
+# 2. Prevent NA's from being allowed in column
 
 cars <- as_tibble(mtcars)
-cars <- factor_conversion(
-  df = cars,
-  columns = c('cyl', 'vs', 'am', 'gear', 'carb')
-)
+# cars['am'][1:5,][[1]] = NA
+cars <- as_factor_dataframe(df = cars, columns = c('cyl', 'vs', 'am', 'gear', 'carb'))
 
-print(cars)
+# Run Model ---------------------------------------------------------------
+cars_model <- build_model(cars, y = 'carb', x = 'am')
+print(summary(cars_model))
+
 
